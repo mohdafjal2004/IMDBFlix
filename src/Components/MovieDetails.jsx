cd54023a7007a9540437c4e88c1f6924f3574ec6
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-03-26 18:30:43 +0530

    Added all details of movie and similar movies in MovieDetails components

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -8,30 +8,51 @@ const MovieDetails = () => {
   const { id } = useParams();
   const [Api, setApi] = useState("");
   const [cast, setCast] = useState([]);
-  console.log(cast);
+  const [same, setSame] = useState([]);
+  console.log(same);
 
   const fetchDetails = () => {
-    const res = axios
+    axios
       .get(`/movie/${id}?api_key=${key}`)
-      .then((response) => setApi(response.data));
+      .then((response) => setApi(response.data))
+      // .then((abc) => console.log(abc?.genres.name));
   };
 
   const fetchCast = () => {
-    const res = axios
+    axios
       .get(`/movie/${id}/credits?api_key=${key}`)
       .then((response) => setCast(response.data));
+  };
+
+  const similar = () => {
+    axios
+      .get(`/movie/${id}/similar?api_key=${key}`)
+      .then((response) => setSame(response.data));
   };
 
   useEffect(() => {
     fetchDetails();
     fetchCast();
+    similar();
   }, [id]);
+  const rating = Math.round(Api.vote_average);
 
   return (
     <div>
       <h1>Movie details of id: {id} here</h1>
+      <h3>{Api && Api.tagline}</h3>
+      <p> Rating: {rating}/10</p>
       <h1>{Api && Api.original_title}</h1>
+      <h1>Release Date : {Api && Api.release_date}</h1>
+      <h1>Run time : {Api && Api.runtime} min</h1>
       <img src={`${Banner_Base_Url}/${Api.backdrop_path}`} alt="" />
+
+      <div>
+        {Api &&
+          Api.genres?.map((genre) => {
+            return <p key={genre.id}>{genre.name}</p>;
+          })}
+      </div>
       <div>
         {cast &&
           cast.cast?.map((data) => {
@@ -42,6 +63,20 @@ const MovieDetails = () => {
             );
           })}
       </div>
+
+
+      <div>
+        {same.results?.map((movie) => {
+        return  (
+          <div>
+        <h1>{movie.original_title}</h1>
+        <img src={`${Banner_Base_Url}/${movie.backdrop_path}`} alt="" />;
+
+        </div>
+        )})}
+      </div>
+
+
     </div>
   );
 };
