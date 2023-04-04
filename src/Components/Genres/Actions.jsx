c84efd723ca23950d9adeb3e121bbae2a38a84d8
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-04 12:15:17 +0530

    Rendered  Action  Movies on Homepage

--- src/Components/Genres/Actions.jsx
+++ src/Components/Genres/Actions.jsx
@@ -1,8 +1,21 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "../../Utils/API";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Action = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: true,
+  };
   const image_Base_Url = "https://image.tmdb.org/t/p/w500";
 
   const [details, setDetails] = useState();
@@ -19,21 +32,29 @@ const Action = () => {
   }, []);
 
   return (
-    <h1>
-      Genres Movies here
-      {details &&
-        details?.results &&
-        details?.results.map((movie) => {
-          return (
-            <div key={movie.id}>
-              <h1>{movie.original_title}</h1>
-              <Link to={`/category/${movie.id}`}>
-                <img src={`${image_Base_Url}/${movie.poster_path}`} alt="" />
-              </Link>
-            </div>
-          );
-        })}
-    </h1>
+    <div className="ml-6 mr-5 mt-8">
+      <h1 className="text-white text-left mb-3 font- font-bold text-2xl text">
+        Action Movies
+      </h1>
+      <Slider {...settings}>
+        {details &&
+          details?.results &&
+          details?.results.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <h1>{movie.original_title}</h1>
+                <Link to={`/category/${movie.id}`}>
+                  <img
+                    src={`${image_Base_Url}/${movie.poster_path}`}
+                    alt=""
+                    className="h-[350px] w-[250px] rounded hover:scale-95 translate-x-2 ease duration-200"
+                  />
+                </Link>
+              </div>
+            );
+          })}
+      </Slider>
+    </div>
   );
 };
 
