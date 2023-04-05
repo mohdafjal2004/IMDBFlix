5c4c4f8e6b1d02d43a646c599996d3842b2572d9
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-05 11:41:07 +0530

    Added images of cast of a movie

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,9 +1,78 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "../Utils/API";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const key = process.env.REACT_APP_API_KEY;
 
 const MovieDetails = () => {
+  var settings = {
+   
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: true,
+
+    responsive: [
+      {
+        breakpoint: 1536,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const Banner_Base_Url = "https://image.tmdb.org/t/p/w500";
   const { id } = useParams();
   const [Api, setApi] = useState("");
@@ -93,15 +162,27 @@ const MovieDetails = () => {
         </div>
       </div>
 
-      <div className="bg-slate-300">
-        {cast &&
-          cast.cast?.map((data) => {
-            return (
-              <Link to={`/castDetails/${data.id}`} key={data.id}>
-                {data.name}
-              </Link>
-            );
-          })}
+      <div className="ml-12  sm:ml-6 duo:ml-6 mr-5 mt-8 text-center ">
+        <h1 className="text-white text-left mb-3 font- font-bold text-2xl text">
+          Popular on Netflix
+        </h1>
+        <Slider {...settings}>
+          {cast &&
+            cast.cast?.map((data) => {
+              return (
+                <Link to={`/castDetails/${data.id}`} key={data.id}>
+                  <div>
+                    <p>{data.name}</p>
+                    <img
+                      src={`${Banner_Base_Url}/${data.profile_path}`}
+                      alt={data.profile_path}
+                      className="h-[300px] w-[250px] duo:w-[200px] 2xl: sixh:w-[180px]  2xl:w-[250px] rounded-lg hover:scale-95 translate-x-2 ease duration-200 "
+                    />
+                  </div>
+                </Link>
+              );
+            })}
+        </Slider>
       </div>
 
       <div>
