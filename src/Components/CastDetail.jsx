b201c43f97b12fed0061d734a536142069f12575
Mohd Afjal <mohdafjalgci2018@gmail.com>
2024-04-13 23:37:44 +0530

    Complete Cast and Movie Details

--- src/Components/CastDetail.jsx
+++ src/Components/CastDetail.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "../Utils/API";
-import bg from './Assets/bg.jpg'
+import poster from "./Assets/else.jpg";
+
 const key = process.env.REACT_APP_API_KEY;
 const image_Base_Url = "https://image.tmdb.org/t/p/w500";
-
 
 const CastDetails = () => {
   const [details, setDetails] = useState([]);
@@ -43,10 +43,7 @@ const CastDetails = () => {
   return (
     <div className="parent ">
       {/* Fetching Details of the Cast */}
-      <div
-        className="cast_detail_outside  sm:flex  bg-slate-600   h-auto pb-10 bg-cover"
-       
-      >
+      <div className="cast_detail_outside  sm:flex sm:items-start  bg-slate-800   h-auto pb-10 bg-cover pt-6 md:pt-16">
         <div className="cast_detail_inside  sm:py-10 " />
         <div className="relative    py-10 px-6 pl-10 fourh:pl-16">
           <img
@@ -56,7 +53,7 @@ const CastDetails = () => {
           />
         </div>
 
-        <div className="text-white sm:w-2/3  sm:mt-16 px-6 pl-10 text-left">
+        <div className="text-white sm:w-2/3  pt-10 px-6 pl-10 text-left">
           <h2 className="text-sm font-roboto font-semibold">
             {details.known_for_department}
           </h2>
@@ -74,16 +71,19 @@ const CastDetails = () => {
       </div>
 
       {/* Fetching all movies of the cast */}
-      <div className="ml-12  sm:ml-6 duo:ml-6 mr-5 mt-14  ">
+      {/* <div className="ml-12  sm:ml-6 duo:ml-6 mr-5 mt-14  ">
         <h1 className="text-white font-roboto text-2xl duo:text-3xl duo:text-left  font-extrabold sm:ml-7 mb-10">
-          Similar Movies
+          Related Movies
         </h1>
         <div className="mx-2 grid md:grid-cols-4 duo:grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 ">
           {first50 &&
             first50.map((movies) => {
               return (
                 <div key={movies.id}>
-                  <Link to={`/category/${movies.id}`}>
+                  <Link
+                    to={`/category/${movies.id}`}
+                    className="md:flex md:flex-col w-screen md:w-full  relative  "
+                  >
                     <img
                       src={`${image_Base_Url}/${movies.poster_path}`}
                       alt=""
@@ -92,6 +92,41 @@ const CastDetails = () => {
                     <h3 className="text-white grid justify-items-center pr-4 font-roboto font-bold mb-10">
                       {movies.original_title}
                     </h3>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div> */}
+      <div className="my-10 md:w-[99vw] text-center  w-screen  md:px-4  2xl:px-28">
+        <h1 className="text-white font-roboto text-2xl mb-4 md:text-left md:text-3xl md:px-7 lg:px-11  xl:px-9 2xl:px-2  font-extrabold">
+          Related Movies
+        </h1>
+        <div className=" flex flex-col  items-center justify-center gap-4 md:grid md:grid-cols-4 xl:grid-cols-5 md:gap-6 md:justify-items-center">
+          {first50 &&
+            first50?.map((movies) => {
+              return (
+                <div key={movies.id}>
+                  <Link
+                    to={`/category/${movies.id}`}
+                    className="md:flex md:flex-col w-screen md:w-full  relative  "
+                  >
+                    {movies.poster_path === null ? (
+                      <img
+                        src={poster}
+                        alt={movies.poster_path}
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95    opacity-75 "
+                      />
+                    ) : (
+                      <img
+                        src={`${image_Base_Url}/${movies.poster_path}`}
+                        alt="Cast profile not available"
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95     opacity-75 "
+                      />
+                    )}
+                    <h1 className="text-white grid justify-items-center mt-2 mb-4 pr-4 font-roboto font-bold line-clamp-1">
+                      {movies.original_title}
+                    </h1>
                   </Link>
                 </div>
               );
--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,94 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "../Utils/API";
-import Slider from "react-slick";
 import man from "./Assets/man.png";
 import poster from "./Assets/else.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import arrowIcon from "./Assets/arrow.png";
 const key = process.env.REACT_APP_API_KEY;
 
 const MovieDetails = () => {
-  var settings = {
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    pauseOnHover: true,
-
-    responsive: [
-      {
-        breakpoint: 1536,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-          adaptiveHeight: true,
-        },
-      },
-      {
-        breakpoint: 1170,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   const Banner_Base_Url = "https://image.tmdb.org/t/p/w500";
   const image_Base_Url = "https://image.tmdb.org/t/p/original";
 
   const { id } = useParams();
   const [Api, setApi] = useState("");
-  console.log(Api)
+  const castItemRef = useRef(null);
   const [cast, setCast] = useState([]);
   const [same, setSame] = useState([]);
-  console.log(same);
 
   const fetchDetails = () => {
     axios
       .get(`/movie/${id}?api_key=${key}&language=en-US`)
       .then((response) => setApi(response.data));
-    // .then((abc) => console.log(abc?.genres.name));
   };
 
   const fetchCast = () => {
@@ -112,9 +45,22 @@ const MovieDetails = () => {
   const rating = Math.round(Api.vote_average);
   const bgImage = `${image_Base_Url}${Api.backdrop_path}`;
 
+  const handleLeftScroll = () => {
+    castItemRef.current.scrollTo({
+      left: castItemRef.current.scrollLeft - castItemRef.current.clientWidth,
+      behavior: "smooth",
+    });
+  };
+  const handleRightScroll = () => {
+    castItemRef.current.scrollTo({
+      left: castItemRef.current.scrollLeft + castItemRef.current.clientWidth,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="parent ">
-      <div className="movie_detail_outside  h-auto  ">
+      <div className="movie_detail_outside  h-auto  overflow-hidden ">
+        {/* Movie Details Section */}
         <div
           className="movie_detail_inside sm:flex sm:flex-row flex flex-col  sm:h-[500px] h-auto pt-16 md:pt-24 lg:pt-28 
           w-screen md:p-10 md:pl-32  items-center py-8 px-2 sm:p-7 relative  bg-center md:bg-top bg-cover before:absolute before:-inset-1 before:bg-gradient-to-t before:from-black"
@@ -168,72 +114,109 @@ const MovieDetails = () => {
         </div>
       </div>
 
-      <div className="ml-12  sm:ml-6 duo:ml-6 mr-5 mt-14 mb-7 text-center ">
-        <h1 className="text-white font-roboto text-2xl duo:text-3xl sm:text-left  font-extrabold">
+      {/* All Casts */}
+      <div className=" relative  my-10  md:w-[99vw]   text-center  w-screen  md:px-4  2xl:px-28  md:overflow-hidden">
+        <h1 className="text-white font-roboto text-2xl mb-4 md:text-left md:text-3xl md:px-7 lg:px-11  xl:px-9 2xl:px-2  font-extrabold">
           Cast
         </h1>
-        <Slider {...settings}>
+        <div
+          className=" flex overflow-auto scrollbar-hide overflow-y-hidden "
+          ref={castItemRef}
+        >
           {cast &&
             cast.cast?.map((data) => {
               return (
-                <Link to={`/castDetails/${data.id}`} key={data.id}>
-                  <div className="relative">
-                    <p>{data.name}</p>
+                <div
+                  key={data.id}
+                  className="md:flex w-screen md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6  relative  border-fuchsia-600"
+                >
+                  <Link
+                    to={`/castDetails/${data.id}`}
+                    className="   border-green-400  w-screen  px-9 flex justify-center "
+                  >
+                    {/* Image */}
                     {data.profile_path === null ? (
                       <img
                         src={man}
                         alt={data.profile_path}
-                        className="h-[300px] w-[250px] duo:w-[200px] 2xl: sixh:w-[180px]  2xl:w-[250px] rounded-lg hover:scale-95 translate-x-2 ease duration-200   opacity-75"
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95     opacity-75 "
                       />
                     ) : (
                       <img
                         src={`${Banner_Base_Url}/${data.profile_path}`}
                         alt="Cast profile not available"
-                        className="h-[300px] w-[250px] duo:w-[200px] 2xl: sixh:w-[180px]  2xl:w-[250px] rounded-lg hover:scale-95 translate-x-2 ease duration-200    opacity-75"
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95      opacity-75"
                       />
                     )}
+                    {/*div for text on image */}
                     <div className="text-white flex flex-col absolute top-10 px-20 pt-52  ">
                       <p className=" font-roboto font-semibold">
                         {data.original_name}
                       </p>
                       <p className=" font-roboto ">{data.character}</p>
                     </div>
-                  </div>
-                </Link>
+                  </Link>
+                </div>
               );
             })}
-        </Slider>
+          <button
+            className="absolute top-[50%] left-0"
+            onClick={handleLeftScroll}
+          >
+            <img
+              src={arrowIcon}
+              alt="left_arrow_icon"
+              className="rotate-90 h-6 w-6 border-2 rounded-full p-1 box-content"
+            />
+          </button>
+          <button
+            className="absolute top-[50%] right-0"
+            onClick={handleRightScroll}
+          >
+            <img
+              src={arrowIcon}
+              alt="left_arrow_icon"
+              className="-rotate-90 h-6 w-6 border-2 rounded-full p-1 box-content"
+            />
+          </button>
+        </div>
+        {/* button for left and right scroll */}
       </div>
 
-      <div className="ml-12  sm:ml-6 duo:ml-6 mr-5 mt-14 ">
-        <h1 className="text-white font-roboto text-2xl duo:text-3xl sm:text-left  font-extrabold">
+      {/* Similar Movies */}
+      <div className="my-10 md:w-[99vw] text-center  w-screen  md:px-4  2xl:px-28">
+        <h1 className="text-white font-roboto text-2xl mb-4 md:text-left md:text-3xl md:px-7 lg:px-11  xl:px-9 2xl:px-2  font-extrabold">
           Similar Movies
         </h1>
-        <div className="mx-2 grid md:grid-cols-4 duo:grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 ">
-          {same.results?.map((movie) => {
-            return (
-              <div key={movie.id}>
-                <Link to={`/category/${movie.id}`}>
-                  {movie.poster_path === null ? (
-                    <img
-                      src={poster}
-                      alt={movie.poster_path}
-                      className="h-[300px] w-[250px] duo:w-[200px] 2xl: sixh:w-[180px]  2xl:w-[250px] rounded-lg hover:scale-95 translate-x-2 ease duration-200  "
-                    />
-                  ) : (
-                    <img
-                      src={`${Banner_Base_Url}/${movie.poster_path}`}
-                      alt="Cast profile not available"
-                      className="h-[300px] w-[250px] duo:w-[200px] 2xl: sixh:w-[180px]  2xl:w-[250px] rounded-lg hover:scale-95 translate-x-2 ease duration-200    opacity-75"
-                    />
-                  )}
-                  <h1 className="text-white grid justify-items-center pr-4 font-roboto font-bold">
-                    {movie.original_title}
-                  </h1>
-                </Link>
-              </div>
-            );
-          })}
+        <div className=" flex flex-col  items-center justify-center gap-4 md:grid md:grid-cols-4 xl:grid-cols-5 md:gap-6 md:justify-items-center">
+          {same &&
+            same.results?.map((movie) => {
+              return (
+                <div key={movie.id}>
+                  <Link
+                    to={`/category/${movie.id}`}
+                    className="md:flex md:flex-col w-screen md:w-full  relative  "
+                  >
+                    {movie.poster_path === null ? (
+                      <img
+                        src={poster}
+                        alt={movie.poster_path}
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95    opacity-75 "
+                      />
+                    ) : (
+                      <img
+                        src={`${Banner_Base_Url}/${movie.poster_path}`}
+                        alt="Cast profile not available"
+                        className="  h-[300px]  w-[80vw]   rounded-lg hover:scale-95     opacity-75 "
+                      />
+                    )}
+                    <h1 className="text-white grid justify-items-center pr-4 font-roboto font-bold line-clamp-1">
+                      {movie.original_title}
+                    </h1>
+                  </Link>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
